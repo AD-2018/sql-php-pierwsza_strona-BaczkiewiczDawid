0334d280f26f2e00e04884b721afb562fcf70f60
BaczkiewiczDawid <61363665+BaczkiewiczDawid@users.noreply.github.com>
2020-10-29 14:13:09 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,35 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello world</h1>
-</body>
-</html>
-
 <?php
-    $servername = "172.16.131.125";
-    $username = "z_ghj";
-    $password = "cxz";
-    $dbname = "z_ghj";
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    $sql = "SELECT imie, zarobki, data_urodzenia FROM pracownicy";
-
-    echo("<h1>Zadnaie 1</h1>");
-    echo("<h2>".$sql."</h2>");
-
-    $result = mysqli_query($conn, $sql);
-
-    echo('<table border="1" class="tabelka_moja">');
-        while($row = mysqli_fetch_assoc($result)) {
-            echo("<tr>");
-            echo("<td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
-            echo("</tr>");
-        };
-    echo("</table>");
+echo "Witaj świecie Nazywam sie Jan Kowalski"
 ?>
