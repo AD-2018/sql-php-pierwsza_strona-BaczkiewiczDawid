0131d1b857e5e83dbd04ebd2203eda25f9c0d468
BaczkiewiczDawid <61363665+BaczkiewiczDawid@users.noreply.github.com>
2020-11-06 12:00:39 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -16,8 +16,8 @@
 </html>
 <?php
     require_once("connect.php");
-    
-    $sql = "SELECT * FROM pracownicy, organizacja WHERE id_org = dzial AND dzial = 2";
+
+    $sql = "SELECT * FROM pracownicy WHERE dzial = 2";
     $result = mysqli_query($conn, $sql);
 
     echo("<h1>Zadanie 1</h1>");
@@ -34,7 +34,7 @@
 
     //------------------------------------
 
-    $sql = "SELECT * FROM pracownicy, organizacja WHERE id_org = dzial AND (dzial = 2 OR dzial = 3)";
+    $sql = "SELECT * FROM pracownicy WHERE (dzial = 2 OR dzial = 3)";
     $result = mysqli_query($conn, $sql);
 
     echo("<h1>Zadanie 2</h1>");
@@ -44,14 +44,14 @@
     echo("<th>ID</th><th>Imie</th><th>dzial</th><th>zarobki</th><th>Data urodzenia</th>");
         while($row = mysqli_fetch_assoc($result)) {
             echo("<tr>");
-            echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>");
+            echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
             echo("</tr>");
         };
     echo("</table>");
 
     //-----------------------------------
 
-    $sql = "SELECT * FROM pracownicy, organizacja WHERE id_org = dzial AND zarobki < 30";
+    $sql = "SELECT * FROM pracownicy WHERE zarobki < 30";
     $result = mysqli_query($conn, $sql);
 
     echo("<h1>Zadanie 3</h1>");
@@ -61,7 +61,7 @@
     echo("<th>ID</th><th>Imie</th><th>dzial</th><th>zarobki</th><th>Data urodzenia</th>");
         while($row = mysqli_fetch_assoc($result)) {
             echo("<tr>");
-            echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>");
+            echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
             echo("</tr>");
         };
     echo("</table>");
