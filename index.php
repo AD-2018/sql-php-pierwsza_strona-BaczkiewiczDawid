ff041e306f5b299849ff0a0349e12d4bdc2e7969
BaczkiewiczDawid <61363665+BaczkiewiczDawid@users.noreply.github.com>
2020-11-06 11:45:47 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -15,4 +15,52 @@
 </body>
 </html>
 <?php
+    $sql = "SELECT * FROM pracownicy WHERE dzial = 2";
+    $result = mysqli_query($conn, $sql);
+
+    echo("<h1>Zadanie 1</h1>");
+    echo("<h2>".$sql."</h2>");
+
+    echo("<table border='1'>");
+    echo("<th>ID</th><th>Imie</th><th>Zarobki</th><th>Data Urodzenia</th><th>Dzial</th><th>Nazwa dzial</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
+
+    //------------------------------------
+
+    $sql = "SELECT * FROM pracownicy WHERE (dzial = 2 OR dzial = 3)";
+    $result = mysqli_query($conn, $sql);
+
+    echo("<h1>Zadanie 2</h1>");
+    echo("<h2>".$sql."</h2>");
+
+    echo("<table border='1'>");
+    echo("<th>ID</th><th>Imie</th><th>Zarobki</th><th>Data Urodzenia</th><th>Dzial</th><th>Nazwa dzial</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
+
+    //-----------------------------------
+
+    $sql = "SELECT * FROM pracownicy WHERE zarobki < 30;
+    $result = mysqli_query($conn, $sql);
+
+    echo("<h1>Zadanie 3</h1>");
+    echo("<h2>".$sql."</h2>");
+
+    echo("<table border='1'>");
+    echo("<th>ID</th><th>Imie</th><th>Zarobki</th><th>Data Urodzenia</th><th>Dzial</th><th>Nazwa dzial</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
 ?>
