0395b03c025e4e1c469d9875f9dddcd28ca50642
BaczkiewiczDawid <61363665+BaczkiewiczDawid@users.noreply.github.com>
2020-11-12 14:13:17 +0100

    Add files via upload

--- funkAgregujaca.php
+++ funkAgregujaca.php
@@ -45,4 +45,127 @@
 
     //-------------------------------------------------------------
 
+    $sql = "SELECT SUM(zarobki) as suma FROM pracownicy, organizacja WHERE id_org = dzial AND imie LIKE '%a'";
+
+    $result = mysqli_query($conn, $sql);
+
+    echo("<h1>Zadanie 2</h1>");
+    echo("<h2>".$sql."</h2>");
+
+    echo("<table border='1'>");
+    echo("<th>Suma</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['suma']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
+
+    //-------------------------------------------------------------
+
+    $sql = "SELECT SUM(zarobki) as suma FROM pracownicy, organizacja WHERE id_org = dzial AND imie NOT LIKE '%a' AND (dzial = 2 OR dzial = 3)";
+
+    $result = mysqli_query($conn, $sql);
+
+    echo("<h1>Zadanie 3</h1>");
+    echo("<h2>".$sql."</h2>");
+
+    echo("<table border='1'>");
+    echo("<th>Suma</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['suma']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
+
+    //-------------------------------------------------------------
+
+    $sql = "SELECT AVG(zarobki) as sr FROM pracownicy, organizacja WHERE id_org = dzial AND imie NOT LIKE '%a'";
+
+    $result = mysqli_query($conn, $sql);
+
+    echo("<h1>Zadanie 4</h1>");
+    echo("<h2>".$sql."</h2>");
+
+    echo("<table border='1'>");
+    echo("<th>Średnia</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['sr']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
+
+    //-------------------------------------------------------------
+
+    $sql = "SELECT AVG(zarobki) as sr FROM pracownicy, organizacja WHERE id_org = dzial AND dzial = 4";
+
+    $result = mysqli_query($conn, $sql);
+
+    echo("<h1>Zadanie 5</h1>");
+    echo("<h2>".$sql."</h2>");
+
+    echo("<table border='1'>");
+    echo("<th>Średnia</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['sr']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
+
+    //-------------------------------------------------------------
+
+    $sql = "SELECT AVG(zarobki) as sr FROM pracownicy, organizacja WHERE id_org = dzial AND imie NOT LIKE '%a' AND (dzial = 1 OR dzial = 2)";
+
+    $result = mysqli_query($conn, $sql);
+
+    echo("<h1>Zadanie 6</h1>");
+    echo("<h2>".$sql."</h2>");
+
+    echo("<table border='1'>");
+    echo("<th>Średnia</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['sr']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
+
+    //-------------------------------------------------------------
+
+    $sql = "SELECT count(imie) as liczba_pracownikow FROM pracownicy, organizacja WHERE id_org = dzial";
+
+    $result = mysqli_query($conn, $sql);
+
+    echo("<h1>Zadanie 4</h1>");
+    echo("<h2>".$sql."</h2>");
+
+    echo("<table border='1'>");
+    echo("<th>Liczba pracownikow</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['liczba_pracownikow']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
+
+    //-------------------------------------------------------------
+
+    $sql = "SELECT count(imie) as liczba_pracownikow FROM pracownicy, organizacja WHERE id_org = dzial AND imie LIKE '%a' AND (dzial = 1 OR dzial = 3)";
+
+    $result = mysqli_query($conn, $sql);
+
+    echo("<h1>Zadanie 4</h1>");
+    echo("<h2>".$sql."</h2>");
+
+    echo("<table border='1'>");
+    echo("<th>liczba kobiet</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['liczba_pracownikow']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
 ?>
