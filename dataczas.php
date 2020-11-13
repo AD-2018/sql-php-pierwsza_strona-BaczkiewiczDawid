82c83ec7b6df524a6a4af961bd5691e171589541
BaczkiewiczDawid <61363665+BaczkiewiczDawid@users.noreply.github.com>
2020-11-13 11:44:09 +0100

    Add files via upload

--- dataczas.php
+++ dataczas.php
@@ -326,4 +326,22 @@
             echo("</tr>");
         };
     echo("</table>");
+
+    //--------------------------------------------
+
+    $sql = "SELECT imie,DATEDIFF(CURDATE(),data_urodzenia) as dni, DATEDIFF(CURDATE(),data_urodzenia)*24 as godziny, DATEDIFF(CURDATE(),data_urodzenia)*24*60 as minuty FROM pracownicy";
+
+    $result = mysqli_query($conn, $sql);
+        
+    echo("<h1>Zadanie 6</h1>");
+    echo("<h2>".$sql."</h2>");
+        
+    echo("<table border='1'>");
+    echo("<th>ID</th><th>Imie</th><th>dzial</th><th>zarobki</th><th>Data urodzenia</th><th>dni</th><th>godziny</th><th>minuty</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['dni']."</td><td>".$row['godziny']."</td><td>".$row['minuty']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
 ?>
