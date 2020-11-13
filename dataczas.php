2caca253e0821a0b7055d7256f87fccfa2053d51
BaczkiewiczDawid <61363665+BaczkiewiczDawid@users.noreply.github.com>
2020-11-13 10:39:11 +0100

    Add files via upload

--- dataczas.php
+++ dataczas.php
@@ -144,4 +144,112 @@
             echo("</tr>");
         };
     echo("</table>");
+
+    //-------------------------------------------
+
+    $sql = "SELECT sm(YEAR(curdate())-YEAR(data_urodzenia)) AS wiek, dzial, nazwa_dzial FROM pracownicy, organizacja WHERE id_org = dzial GROUP BY dzial";
+
+    $result = mysqli_query($conn, $sql);
+
+    echo("<h1>Zadanie 8</h1>");
+    echo("<h2>".$sql."</h2>");
+
+    echo("<table border='1'>");
+    echo("<th>suma lat</th><th>Dzial</th><th>Nazwa dzial</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['wiek']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
+
+    //-------------------------------------------
+
+    $sql = "SELECT MAX(YEAR(CURDATE()) - YEAR(data_urodzenia)) as wiek, dzial, nazwa_dzial from pracownicy,organizacja WHERE id_org = dzial GROUP BY dzial";
+
+    $result = mysqli_query($conn, $sql);
+
+    echo("<h1>Zadanie 9</h1>");
+    echo("<h2>".$sql."</h2>");
+
+    echo("<table border='1'>");
+    echo("<th>najstarsi</th><th>Dzial</th><th>Nazwa dzial</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['wiek']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
+
+    //-------------------------------------------
+
+    $sql = "SELECT MIN(YEAR(CURDATE()) - YEAR(data_urodzenia)) as wiek, dzial, nazwa_dzial from pracownicy,organizacja WHERE id_org = dzial AND (dzial = 'handel' OR dzial = 'serwis') GROUP BY dzial";
+
+    $result = mysqli_query($conn, $sql);
+
+    echo("<h1>Zadanie 10</h1>");
+    echo("<h2>".$sql."</h2>");
+
+    echo("<table border='1'>");
+    echo("<th>najmlodszy</th><th>Dzial</th><th>Nazwa dzial</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['wiek']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
+
+    //--------------------------------------------
+
+    $sql = "SELECT MIN(YEAR(CURDATE()) - YEAR(data_urodzenia)) as wiek, dzial, nazwa_dzial, imie from pracownicy,organizacja WHERE id_org = dzial GROUP BY dzial";
+
+    $result = mysqli_query($conn, $sql);
+
+    echo("<h1>Zadanie 11</h1>");
+    echo("<h2>".$sql."</h2>");
+
+    echo("<table border='1'>");
+    echo("<th>Imie</th><th>najmlodszy</th><th>Dzial</th><th>Nazwa dzial</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['imie']."</td><td>".$row['wiek']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
+
+    //--------------------------------------------
+
+    $sql = "SELECT imie, DATEDIFF(CURDATE(),data_urodzenia) AS dni_zycia FROM pracownicy";
+
+    $result = mysqli_query($conn, $sql);
+
+    echo("<h1>Zadanie 12</h1>");
+    echo("<h2>".$sql."</h2>");
+
+    echo("<table border='1'>");
+    echo("<th>Imie</th><th>dni zycia</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['imie']."</td><td>".$row['dni_zycia']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
+
+    //--------------------------------------------
+
+    $sql = "SELECT * FROM pracownicy WHERE imie NOT LIKE '%a' ORDER BY data_urodzenia ASC LIMIT 1";
+
+    $result = mysqli_query($conn, $sql);
+    
+    echo("<h1>Zadanie 13</h1>");
+    echo("<h2>".$sql."</h2>");
+    
+    echo("<table border='1'>");
+    echo("<th>ID</th><th>Imie</th><th>dzial</th><th>zarobki</th><th>Data urodzenia</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
 ?>
