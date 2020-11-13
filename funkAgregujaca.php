0e6980b93d15ddea7a7a44940e54b78864086778
BaczkiewiczDawid <61363665+BaczkiewiczDawid@users.noreply.github.com>
2020-11-13 08:57:44 +0100

    Add files via upload

--- funkAgregujaca.php
+++ funkAgregujaca.php
@@ -265,7 +265,7 @@
 
     //-------------------------------------------------------------
 
-    echo("<h1>klauzula HAVING</h1>")
+    echo("<h1>klauzula HAVING</h1>");
 
     $sql = "SELECT sum(zarobki), nazwa_dzial FROM pracownicy, organizacja WHERE id_org = dzial GROUP BY nazwa_dzial HAVING sum(zarobki) < 28";
 
