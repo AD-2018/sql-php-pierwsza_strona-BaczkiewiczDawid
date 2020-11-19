0d539983e6c1a1c4a5aa7d99bfe52b1bbf68678a
BaczkiewiczDawid <61363665+BaczkiewiczDawid@users.noreply.github.com>
2020-11-19 14:41:27 +0100

    Add files via upload

--- insert.php
+++ insert.php
@@ -1,22 +1,25 @@
 <?php
-require_once("connect.php");
-
 echo("jestes w insert.php");
 echo $_POST['name'];
+
+$servername = "remotemysql.com";
+$username = "FkhGuWFtGI";
+$password = "9kRwNvqeLf";
+$dbname = "FkhGuWFtGI";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-};
+}
 
-$sql = "INSERT INTO pracownicy (null, $POST['name'], dzial, zarobki) 
-       VALUES (null, 'Ksawery', 3, 36,'1995-10-21')";
+$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
+       VALUES (null, $_POST['name'], 1, 76,'1991-11-21')";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
-};
+}
 
 $conn->close();
 ?>
