e6eb5f8d4cefa29767e3c737dadac71d4b44da1c
BaczkiewiczDawid <61363665+BaczkiewiczDawid@users.noreply.github.com>
2020-11-19 14:37:09 +0100

    Add files via upload

--- insert.php
+++ insert.php
@@ -7,16 +7,16 @@ echo $_POST['name'];
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-}
+};
 
-$sql = "INSERT INTO pracownicy (null, $POST['name], dzial, zarobki) 
+$sql = "INSERT INTO pracownicy (null, $POST['name'], dzial, zarobki) 
        VALUES (null, 'Ksawery', 3, 36,'1995-10-21')";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
-}
+};
 
 $conn->close();
 ?>
