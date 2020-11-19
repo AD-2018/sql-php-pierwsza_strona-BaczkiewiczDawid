6b261a05b9e6b820def9336694417dd6f1c3e2db
BaczkiewiczDawid <61363665+BaczkiewiczDawid@users.noreply.github.com>
2020-11-19 14:24:02 +0100

    Add files via upload

--- insert.php
+++ insert.php
@@ -6,7 +6,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO Pracownik (null, imie, dzial, zarobki) 
+$sql = "INSERT INTO pracownicy (null, imie, dzial, zarobki) 
        VALUES (null,'Ksawery', 3, 36,'1995-10-21')";
 
 if ($conn->query($sql) === TRUE) {
