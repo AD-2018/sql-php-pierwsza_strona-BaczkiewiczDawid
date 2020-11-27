32d8d8fccff77013ba09263ea538222b3270aa03
BaczkiewiczDawid <61363665+BaczkiewiczDawid@users.noreply.github.com>
2020-11-27 11:55:09 +0100

    Add files via upload

--- danedobazy.php
+++ danedobazy.php
@@ -47,7 +47,7 @@ echo("<th>ID</th><th>Imie</th><th>Zarobki</th><th>Data Urodzenia</th><th>Dzial</
         echo('<td>'.$row['id_pracownicy'].'</td>'.'<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td>'.'<td>'.$row['dzial'].'</td><td>'.$row['nazwa_dzial'].'</td><td>'.$row['data_urodzenia'].'</td>'.
         '<td>
         <form action="delete.php" method="POST">
-            <input type="number" name="id" value="'.$row['id_pracownicy'].' hidden">
+            <input type="number" name="id" value="'.$row['id_pracownicy'].'" hidden>
             <input type="submit" value="X">
         </form>
         </td>');
