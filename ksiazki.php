659f4c046596efe11f0fb1dba7d2c2e2e002e8d5
BaczkiewiczDawid <61363665+BaczkiewiczDawid@users.noreply.github.com>
2020-12-15 20:07:01 +0100

    Add files via upload

--- ksiazki.php
+++ ksiazki.php
@@ -7,18 +7,21 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1 class="name">Dawid Bączkiewicz nr1</h1>
-    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-BaczkiewiczDawid" class="github-link">github</a>
-    <br>
-    <nav class="nav">
-        <a href="index.php">Pracownicy</a>
-        <a href="funkAgregujaca.php">Funkcje agregujące</a>
-        <a href="orgPracownicy.php">Organizacja i pracownicy</a>
-        <a href="dataczas.php">Data i czas</a>
-        <a href="formularz.html">Formularz</a>
-        <a href="danedobazy.php">Dane do bazy</a>
-        <a href="ksiazki.php">Ksiazki</a>
-    </nav>
+    <div class="container">
+        <div class="container__title">
+            <h1 class="name">Dawid Bączkiewicz nr1</h1>
+            <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-BaczkiewiczDawid" class="github-link">github</a>
+        </div>
+        <div class="nav">
+            <a href="index.php">Pracownicy</a>
+            <a href="funkAgregujaca.php">Funkcje agregujące</a>
+            <a href="orgPracownicy.php">Organizacja i pracownicy</a>
+            <a href="dataczas.php">Data i czas</a>
+            <a href="formularz.html">Formularz</a>
+            <a href="danedobazy.php">Dane do bazy</a>
+            <a href="ksiazki.php">Ksiazki</a>
+        </div>
+    </div>
 </body>
 </html>
 <?php
@@ -35,6 +38,10 @@ echo('<select name="autor">');
         echo("</option>"); 
     }
 echo('</select>');
+
+$sql = "SELECT * FROM biblAutor, biblTytul";
+
+$result = mysqli_query($conn, $sql);
 
 echo('<select name="tytul">');
     while($row=mysqli_fetch_assoc($result)){
