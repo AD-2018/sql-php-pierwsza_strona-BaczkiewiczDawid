09b0e28e21b9e7bad3bfeb2494e33abbfce75947
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2021-04-19 09:14:34 +0200

    nav

---
+++ assets/gird_nav.php
@@ -0,0 +1,13 @@
+<a href="/index.php">Pracownicy</a><br>
+<a href="/grid/gridOne.php">Grid1</a><br>
+<a href="/grid/gridTwo.php">Grid2</a><br>
+<a href="/grid/gridThree.php">Grid3</a><br>
+<a href="/grid/gridFour.php">Grid4</a><br>
+<a href="/grid/gridFive.php">Grid5</a><br>
+<a href="/grid/gridSix.php">Grid6</a><br>
+<a href="/grid/gridSeven.php">Grid7</a><br>
+<a href="/grid/gridEight.php">Grid8</a><br>
+<a href="/grid/gridNine.php">Grid9</a><br>
+<a href="/grid/gridTen.php">Grid10</a><br>
+<a href="/grid/gridEleven.php">Grid11</a><br>
+<a href="/grid/gridTwelve.php">Grid12</a><br>
